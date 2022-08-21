b6d917d11936bc9ddc930dbb5f09e735a41bf12a
rastoliska25 <rasto.liska25@gmail.com>
2022-08-21 22:45:27 +0200

    logback - sending logs to logstash

--- src/main/java/com/example/WebClient/WebClientApplication.java
+++ src/main/java/com/example/WebClient/WebClientApplication.java
@@ -2,6 +2,8 @@ package com.example.WebClient;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
@@ -17,6 +19,7 @@ import java.util.stream.Collectors;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "ShopWebClient API", version = "1.0", description = "ShopWebClient microservice"))
+@Slf4j
 public class WebClientApplication {
 
     public static void main(String[] args) {
@@ -24,6 +27,9 @@ public class WebClientApplication {
 
 		SpringApplication.run(WebClientApplication.class, args);
 		System.out.println("test");
+		log.info("test2");
+		log.info("test3");
+		log.info("test4");
 
 
 	}
